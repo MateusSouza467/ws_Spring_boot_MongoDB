a23cb0206aee822f8810e204574c095b58b7474a
MateusSouza467 <mateussouza1903@gmail.com>
2024-09-04 18:32:09 -0300

    First commit

---
+++ src/main/java/com/mateussouza/ws_mongo/WsMongoApplication.java
@@ -0,0 +1,13 @@
+package com.mateussouza.ws_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateussouza/ws_mongo/WsMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateussouza.ws_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
