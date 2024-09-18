17fa122e3226ebda4a24ed12848605c5b3a0b768
MateusSouza467 <mateussouza1903@gmail.com>
2024-09-18 16:25:49 -0300

    Inserção de usuario com POST

--- src/main/java/com/mateussouza/ws_mongo/services/UserService.java
+++ src/main/java/com/mateussouza/ws_mongo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mateussouza.ws_mongo.domain.User;
+import com.mateussouza.ws_mongo.dto.UserDTO;
 import com.mateussouza.ws_mongo.repository.UserRepository;
 import com.mateussouza.ws_mongo.services.exception.ObjectNotFoundException;
 
@@ -20,7 +21,15 @@ public class UserService {
 	}
 	 
 	public User findById(String id) {
-		return repo.findById(id).orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"+ id));
-		
+		return repo.findById(id).orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado"+ id));	
 	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
+	}
+	
 }
