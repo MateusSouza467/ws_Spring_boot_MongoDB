3fc24d4a0b6968512158505d754737bd8d69ddc9
MateusSouza467 <mateussouza1903@gmail.com>
2024-09-22 20:24:43 -0300

    Atualização de usuario com PUT

--- src/main/java/com/mateussouza/ws_mongo/repository/UserRepository.java
+++ src/main/java/com/mateussouza/ws_mongo/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.mateussouza.ws_mongo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,8 @@ import com.mateussouza.ws_mongo.domain.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
+
+	User save(Optional<User> newObj);
 
 
 
